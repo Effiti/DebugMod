067e4d904f07d2304742338fd979d33a66e1109b
Effiti <EffitiDEV@gmail.com>
2024-01-28 18:15:23 +0100

    remove chat message profiling

--- src/main/java/io/github/effiti/edb/mixin/ClientConnectionMixin.java
+++ src/main/java/io/github/effiti/edb/mixin/ClientConnectionMixin.java
@@ -16,15 +16,6 @@ public class ClientConnectionMixin {
 
     @Inject(at = @At("HEAD"), method = "handlePacket", cancellable = true)
     private static <T extends PacketListener> void handlePacket(Packet<T> packet, PacketListener listener, CallbackInfo ci) {
-        /*if(ClientMod.shouldSendPacketSendMessages())
-            ClientMod.sendMessage("SEND: " + packet.getClass().toString());*/
-        if(packet instanceof GameMessageS2CPacket g &&
-                g.content().getString().startsWith("{{")
-        ) {
-            long t_1 = System.currentTimeMillis();
-            long t_0 = Long.parseLong(g.content().getString().substring(2, 12));
-            ClientMod.sendMessage("delta t : "  + (t_1) );
-        }
     }
 
 }
